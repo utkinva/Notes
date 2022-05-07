5b88a585d4a1a5df25f7914342559a78b766946e
utkinva <78167730+utkinva@users.noreply.github.com>
2022-05-07 20:51:31 +0300

    Add a function to undo changes when deleting a note

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const App = {
             placeholderText: 'Введите название заметки',
             inputValue: '',
             notesList: [],
+            deletedNotesList: [],
+            lastItemDeleted: '',
+            lastIndexDeleted: 0,
+            revertBtnClickable: false,
             editItemIndex: 0,
             saveBtnVisible: 'btn primary invisible',
             cancelBtnVisible: 'btn primary invisible danger',
             addBtnVisible: 'btn primary visible',
             toolTipText: 'Максимальная длина названия - 50 символов!',
-            toolTipVisible: false
+            toolTipVisible: false,
+            revertChangesVisible: false
         }
     },
     mounted() {
@@ -22,7 +27,6 @@ const App = {
     methods: {
         addToCookies() {
             window.$cookies.set('cookiesNotesList',JSON.stringify(this.notesList))
-
             this.notesList = JSON.parse(window.$cookies.get('cookiesNotesList'))
         },
         addNewNote() {
@@ -33,12 +37,30 @@ const App = {
             }
 
         },
+        hideRevertChanges() {
+            setTimeout(() => this.revertChangesVisible = false, 2500)
+        },
+        revertChanges() {
+            if (!this.revertBtnClickable) {
+                this.notesList.splice(this.lastIndexDeleted,0, this.lastItemDeleted)
+                this.addToCookies()
+            }
+            this.revertBtnClickable = true
+            this.revertChangesVisible = false
+
+        },
         deleteNote(i) {
+            this.revertBtnClickable = false
+            this.lastIndexDeleted = i
+            this.lastItemDeleted = this.notesList[i]
             this.notesList.splice(i,1)
+            this.deletedNotesList.push(this.notesList[i])
             this.inputValue = ''
             this.addBtnVisible = 'btn primary visible'
             this.saveBtnVisible = 'btn primary invisible'
             this.cancelBtnVisible = 'btn primary invisible danger'
+            this.revertChangesVisible = true
+            this.hideRevertChanges()
             this.addToCookies()
         },
         editNote(i) {
